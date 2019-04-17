f1a880bf15b9dbdd140f7228053a57a51b073c6a
HANA LEANDER <hana0070@gmail.com>
2019-04-17 03:34:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,13 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |dwarves, index|
     puts "#{index + 1}. #{name}"
+  end
+end
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
   end
 end
 
