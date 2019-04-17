1fe22f5c74f045f5e24dd4b223d4dd94e4a9e5c8
HANA LEANDER <hana0070@gmail.com>
2019-04-17 03:35:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,13 @@
+dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
+
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |dwarves, index|
