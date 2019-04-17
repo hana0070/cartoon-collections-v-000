66e2f95729fd6607b57a9cdca1d546699fa62ab7
HANA LEANDER <hana0070@gmail.com>
2019-04-17 03:28:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,6 @@ def roll_call_dwarves(name)# code an argument here
   # Your code here
   roll_call_dwarves.each do |name|
     puts #{name}
-
 end
 
 def summon_captain_planet# code an argument here
